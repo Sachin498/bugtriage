d4eb581424a504e70f72098a4cc9fe618f828ab5
Sachin498 <53938517+Sachin498@users.noreply.github.com>
2020-09-26 12:38:49 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,23 @@
+from dbrnna import run_dbrnna_chronological_cv
+from preprocess import preprocess_all_datasets
+
+
+# preprocess_all_datasets()
+
+gc_result_dict = run_dbrnna_chronological_cv(
+    dataset_name="google_chromium",
+    min_train_samples_per_class=20,
+    num_cv=10,
+    rnn_type="gru",
+)
+print("gc_result_dict:", gc_result_dict)
+
+mc_result_dict = run_dbrnna_chronological_cv(
+    dataset_name="mozilla_core", min_train_samples_per_class=20, num_cv=10
+)
+print("mc_result_dict:", mc_result_dict)
+
+mf_result_dict = run_dbrnna_chronological_cv(
+    dataset_name="mozilla_firefox", min_train_samples_per_class=20, num_cv=10
+)
+print("mf_result_dict:", mf_result_dict)
